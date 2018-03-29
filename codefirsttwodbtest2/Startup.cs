0884384ad25238e21761566bbb396be2d8f045dc
Darryl Challenger <darryl.challenger@outlook.com>
2018-03-29 16:32:04 -0400

    Add project files.

---
+++ codefirsttwodbtest2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(codefirsttwodbtest2.Startup))]
+namespace codefirsttwodbtest2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
